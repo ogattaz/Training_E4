e3fc8d34144e8206eb9da84ad72e48359a35cc6b
ogattaz <olivier@gattaz.com>
2016-07-12 17:40:58 +0200

    Simple view migration : RentalPropertyView

--- com.opcoach.training.rental.ui/src/com/opcoach/training/rental/ui/e4/views/RentalPropertyView.java
+++ com.opcoach.training.rental.ui/src/com/opcoach/training/rental/ui/e4/views/RentalPropertyView.java
@@ -1,17 +1,17 @@
-package com.opcoach.training.rental.ui.views;
+package com.opcoach.training.rental.ui.e4.views;
+
+import javax.annotation.PostConstruct;
 
 import org.eclipse.core.databinding.DataBindingContext;
 import org.eclipse.core.databinding.UpdateValueStrategy;
 import org.eclipse.core.databinding.beans.PojoProperties;
 import org.eclipse.core.databinding.observable.value.IObservableValue;
 import org.eclipse.core.internal.databinding.conversion.DateToStringConverter;
-import org.eclipse.core.runtime.Platform;
+import org.eclipse.e4.ui.di.Focus;
 import org.eclipse.emf.databinding.EMFObservables;
 import org.eclipse.emf.databinding.EMFProperties;
 import org.eclipse.emf.databinding.FeaturePath;
 import org.eclipse.jface.databinding.swt.WidgetProperties;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.dnd.DND;
 import org.eclipse.swt.dnd.DragSource;
@@ -24,11 +24,6 @@ import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.ui.ISelectionListener;
-import org.eclipse.ui.IViewSite;
-import org.eclipse.ui.IWorkbenchPart;
-import org.eclipse.ui.PartInitException;
-import org.eclipse.ui.part.ViewPart;
 
 import com.opcoach.training.rental.Rental;
 import com.opcoach.training.rental.RentalAgency;
@@ -40,10 +35,8 @@ import com.opcoach.training.rental.ui.RentalUIConstants;
 
 /**
  * @author ogattaz
- * @deprecated Use now E4 RentalPropertyView
  */
-@Deprecated
-public class RentalPropertyView extends ViewPart implements ISelectionListener {
+public class RentalPropertyView {
 	public static final String VIEW_ID = "com.opcoach.rental.ui.views.rentalView"; //$NON-NLS-1$
 	private Rental currentRental;
 
@@ -55,10 +48,17 @@ public class RentalPropertyView extends ViewPart implements ISelectionListener {
 	private Label rentedObjectLabel;
 	private Label startDateLabel;
 
+	/**
+	 * 
+	 */
 	public RentalPropertyView() {
+		super();
 	}
 
-	@Override
+	/**
+	 * @param parent
+	 */
+	@PostConstruct
 	public void createPartControl(Composite parent) {
 		parent.setLayout(new GridLayout(1, false));
 
@@ -117,49 +117,57 @@ public class RentalPropertyView extends ViewPart implements ISelectionListener {
 
 	}
 
+	// E34 portage selection listener
 	/*
 	 * (non-Javadoc)
 	 * 
 	 * @see org.eclipse.ui.part.WorkbenchPart#dispose()
 	 */
-	@Override
-	public void dispose() {
-		getSite().getPage().removeSelectionListener(this);
-		super.dispose();
-	}
+	// @Override
+	// public void dispose() {
+	// getSite().getPage().removeSelectionListener(this);
+	// super.dispose();
+	// }
+	//
+	// @Override
+	// public void init(IViewSite site) throws PartInitException {
+	// super.init(site);
+	// site.getPage().addSelectionListener(this);
+	// }
 
-	@Override
-	public void init(IViewSite site) throws PartInitException {
-		super.init(site);
-		site.getPage().addSelectionListener(this);
-	}
-
+	/**
+	 * @return
+	 */
 	protected DataBindingContext initDataBindings() {
 		DataBindingContext bindingContext = new DataBindingContext();
+
 		//
-		IObservableValue observeTextRentedObjectLabelObserveWidget = WidgetProperties.text().observe(rentedObjectLabel);
-		IObservableValue currentRentalNameObserveValue = EMFProperties
+		IObservableValue<?> observeTextRentedObjectLabelObserveWidget = WidgetProperties.text()
+				.observe(rentedObjectLabel);
+		IObservableValue<?> currentRentalNameObserveValue = EMFProperties
 				.value(FeaturePath.fromList(Literals.RENTAL__RENTED_OBJECT, Literals.RENTAL_OBJECT__NAME))
 				.observe(currentRental);
 		bindingContext.bindValue(observeTextRentedObjectLabelObserveWidget, currentRentalNameObserveValue, null, null);
 		//
-		IObservableValue observeTextStartDateLabelObserveWidget = WidgetProperties.text().observe(startDateLabel);
-		IObservableValue startDateCurrentRentalObserveValue = PojoProperties.value("startDate").observe(currentRental);
+		IObservableValue<?> observeTextStartDateLabelObserveWidget = WidgetProperties.text().observe(startDateLabel);
+		IObservableValue<?> startDateCurrentRentalObserveValue = PojoProperties.value("startDate")
+				.observe(currentRental);
 		UpdateValueStrategy strategy = new UpdateValueStrategy();
 		strategy.setConverter(new DateToStringConverter());
 		bindingContext.bindValue(observeTextStartDateLabelObserveWidget, startDateCurrentRentalObserveValue, null,
 				strategy);
 		//
-		IObservableValue observeTextEndDateLabelObserveWidget = WidgetProperties.text().observe(endDateLabel);
-		IObservableValue currentRentalEndDateObserveValue = EMFObservables.observeValue(currentRental,
+		IObservableValue<?> observeTextEndDateLabelObserveWidget = WidgetProperties.text().observe(endDateLabel);
+		IObservableValue<?> currentRentalEndDateObserveValue = EMFObservables.observeValue(currentRental,
 				Literals.RENTAL__END_DATE);
 		UpdateValueStrategy strategy_1 = new UpdateValueStrategy();
 		strategy_1.setConverter(new DateToStringConverter());
 		bindingContext.bindValue(observeTextEndDateLabelObserveWidget, currentRentalEndDateObserveValue, null,
 				strategy_1);
 		//
-		IObservableValue observeTextCustomerNameLabelObserveWidget = WidgetProperties.text().observe(customerNameLabel);
-		IObservableValue customerdisplayNameCurrentRentalObserveValue = PojoProperties.value("customer.displayName")
+		IObservableValue<?> observeTextCustomerNameLabelObserveWidget = WidgetProperties.text()
+				.observe(customerNameLabel);
+		IObservableValue<?> customerdisplayNameCurrentRentalObserveValue = PojoProperties.value("customer.displayName")
 				.observe(currentRental);
 		bindingContext.bindValue(observeTextCustomerNameLabelObserveWidget,
 				customerdisplayNameCurrentRentalObserveValue, null, null);
@@ -167,33 +175,35 @@ public class RentalPropertyView extends ViewPart implements ISelectionListener {
 		return bindingContext;
 	}
 
-	/*
-	 * (non-Javadoc)
+	// E34
+
+	// @Override
+	// public void selectionChanged(IWorkbenchPart part, ISelection selection) {
+	// if (selection.isEmpty())
+	// return;
+	//
+	// if (selection instanceof IStructuredSelection) {
+	// Object sel = ((IStructuredSelection) selection).getFirstElement();
+	//
+	// // La selection courante est elle un Rental ou adaptable en Rental ?
+	// Rental r = Platform.getAdapterManager().getAdapter(sel, Rental.class);
+	// setRental(r);
+	//
+	// }
+	//
+	// }
+
+	/**
 	 * 
-	 * @see org.eclipse.ui.ISelectionListener#selectionChanged(org.eclipse.ui.
-	 * IWorkbenchPart, org.eclipse.jface.viewers.ISelection)
 	 */
-	@Override
-	public void selectionChanged(IWorkbenchPart part, ISelection selection) {
-		if (selection.isEmpty())
-			return;
-
-		if (selection instanceof IStructuredSelection) {
-			Object sel = ((IStructuredSelection) selection).getFirstElement();
-
-			// La selection courante est elle un Rental ou adaptable en Rental ?
-			Rental r = Platform.getAdapterManager().getAdapter(sel, Rental.class);
-			setRental(r);
-
-		}
-
-	}
-
-	@Override
+	@Focus
 	public void setFocus() {
-
+		// E34
 	}
 
+	/**
+	 * @param r
+	 */
 	public void setRental(Rental r) {
 		currentRental = r;
 
